b1ce2f2dba3cd3aa2001f0b817b39b82ddb8cb9e
jin-park0115 <jinpark0115@naver.com>
2026-07-06 16:14:14 +0900

    2차원배

---
+++ src/algorithm/Programmers_181945.java
@@ -0,0 +1,13 @@
+package algorithm;
+import java.util.Scanner;
+public class Programmers_181945 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+		
+		 for(int i = 0 ; i < a.length(); i++){
+	            System.out.println(a.charAt(i));
+        }
+		 // charAt를 잘 쓰자.
+	}
+}
